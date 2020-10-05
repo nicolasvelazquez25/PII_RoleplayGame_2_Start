65c209bd6059e5a541730a23373e0f0dab023851
nicolasvelazquez25 <nicolasvelazquezperez@gmail.com>
2020-10-05 19:25:07 -0300

    Agrego comentarios de los personajes y arreglo problemas en KnightTest

--- src/Library/Characters/Archer.cs
+++ src/Library/Characters/Archer.cs
@@ -2,6 +2,12 @@ namespace RoleplayGame
 {
     public class Archer : Character
     {
+        /*
+        Esta clase hereda de la clase padre Character (buscar su comentario para ver lo heredado), lo que incluye los métodos
+         AttackValue y DefenseValue, en el primero se utiliza unicamente el ataque del Bow, en caso de que el personaje posea uno,
+         al igual que en el caso de DefenseValue se usa unicamente la defensa del Helmet.(En ambos casos se suman los ataques y 
+         defensas, siendo la defensa del Bow cero, al igual que el ataque del Helmet)
+        */
         private int health = 100;
         public Archer(string name)    
                 :base(name)
